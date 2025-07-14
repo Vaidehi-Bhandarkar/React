13fd0d057764cca46c21406267b5b65b6c775e93
Vaidehi Bhandarkar <vaidehikmb@gmail.com>
2025-07-14 16:00:45 +0530

    first commit

---
+++ App.js
@@ -0,0 +1,21 @@
+const heading = React.createElement(
+    "h1", 
+    {id: "heading"}, 
+    "Hello World with React!"
+);
+console.log(heading); //React creates a JS object
+
+const parent = React.createElement(
+    "div",
+    {id: "parent"},
+    
+        React.createElement("div", {id: "child"}, [
+            React.createElement("h1", {}, "H1 Tag"),
+            React.createElement("h2", {}, "H2 Tag")
+        ]),
+    
+)
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+// root.render(heading);
+root.render(parent);
