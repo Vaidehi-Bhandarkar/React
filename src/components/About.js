560e0237b5543b3864e99e3d872a4d1c8c2679a5
Vaidehi Bhandarkar <vaidehikmb@gmail.com>
2025-07-18 16:40:02 +0530

    code changes for swiggy

---
+++ src/components/About.js
@@ -0,0 +1,10 @@
+const About = () => {
+	return (
+		<div>
+			<h1> ABOUT US </h1>
+			<h2> This is About Us </h2>
+		</div>
+	);
+};
+
+export default About;
--- src/components/restaurantCard.js
+++ src/components/restaurantCard.js
@@ -1,18 +1,29 @@
 const RestaurantCard = (props) => {
 	// console.log("This is component: ",props);
 	const { restData } = props;
-	const { name, aggregateRating, servesCuisine, image } = restData?.item; //<- For static data of Zomato
+	const { name, avgRating, cuisines, cloudinaryImageId, sla } = restData?.info; //<- For static data of Zomato
 
 	return (
 		<div className="rest-card">
-			<img
+			{/* ZOMATO STATIC DATA */}
+			{/* <img
 				className="img-logo"
 				alt="Food-img"
 				src={image}
 			/>
 			<h3> {name} </h3>
 			<h4> {aggregateRating?.ratingValue} ⭐️</h4>
-			<h4> {servesCuisine} </h4>
+			<h4> {servesCuisine} </h4> */}
+			{/* SWIGGY DYNAMIC DATA */}
+			<img
+				className="img-logo"
+				alt="Food-img"
+				src={`https://media-assets.swiggy.com/swiggy/image/upload/fl_lossy,f_auto,q_auto,w_660/${cloudinaryImageId}`}
+			/>
+			<h3> {name} </h3>
+			<h4> {avgRating} ⭐️</h4>
+			<h4> {cuisines.join(", ")} </h4>
+			<h4> {sla?.slaString} </h4>
 		</div>
 	);
 };
